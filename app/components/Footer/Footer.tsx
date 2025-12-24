d86d719eff148e7f67201f47ddcc2f4bfe808b78
jyoti <jyoti.baraskar@outlook.com>
2025-12-24 12:43:50 +0400

    remove unused files

--- app/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} Jyoti Baraskar. All rights reserved.
-    </footer>
-  );
-}

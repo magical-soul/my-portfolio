87dd625db00ad35c01a4ca10df0cc1afe14db49d
jyoti <jyoti.baraskar@outlook.com>
2025-12-24 13:05:07 +0400

    add linked in preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,24 @@ export const metadata = {
   title: "Jyoti Baraskar | Frontend Developer",
   description:
     "Frontend Developer specializing in React, Next.js, and modern web performance.",
+
+  openGraph: {
+    title: "Jyoti Baraskar | Frontend Developer",
+    description:
+      "Frontend Developer specializing in React, Next.js, and modern UI.",
+    url: "https://jyotibaraskar-portfolio.vercel.app/",
+    siteName: "Jyoti Baraskar Portfolio",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Jyoti Baraskar Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
